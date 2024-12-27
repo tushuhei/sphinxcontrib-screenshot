851570e400afaf73687aa2bc9587668dfea9e5a8
Éloi Rivard <eloi@yaal.coop>
2024-12-27 04:54:48 +0100

    feat: add pre-commit configuration (#33)

--- doc/conf.py
+++ doc/conf.py
@@ -32,8 +32,6 @@ extensions = ['sphinxcontrib.screenshot']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
