63fbd4a2b192e4d9c035340b3f26dd689e711d78
Éloi Rivard <eloi@yaal.coop>
2025-01-01 22:58:49 +0100

    doc: initial documentation

--- doc/conf.py
+++ doc/conf.py
@@ -1,39 +1,58 @@
-# Copyright 2023 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import datetime
+import os
+import sys
+from importlib import metadata
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../sphinxcontrib_screenshot"))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- General configuration ------------------------------------------------
 
-project = 'sphinxcontrib-screenshot'
-copyright = '2023, Shuhei Iitsuka'
-author = 'Shuhei Iitsuka'
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.graphviz",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.screenshot",
+    "myst_parser",
+]
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+master_doc = "index"
+project = "sphinxcontrib-screenshot"
+year = datetime.datetime.now().strftime("%Y")
+author = "Shuhei Iitsuka"
+copyright = f"{year}, {author}"
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
 
-extensions = ['sphinxcontrib.screenshot']
+version = metadata.version("sphinxcontrib-screenshot")
+language = "en"
+pygments_style = "sphinx"
+todo_include_todos = True
+toctree_collapse = False
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Options for HTML output ----------------------------------------------
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "shibuya"
+html_baseurl = "https://sphinxcontrib-screenshot.readthedocs.io"
+html_theme_options = {
+    "page_layout": "compact",
+    "github_url": "https://github.com/tushuhei/sphinxcontrib-screenshot",
+}
+html_context = {
+    "source_type": "github",
+    "source_user": "tushuhei",
+    "source_repo": "sphinxcontrib-screenshot",
+    "source_version": "main",
+    "source_docs_path": "/doc/",
+}
